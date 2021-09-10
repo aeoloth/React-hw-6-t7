ddff90c00a1013b2d8b0f9a68293d8a5667befd4
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-11 01:39:38 +0300

    first commit

---
+++ hw-6-t7/src/Ccomponent.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+export default class Ccomponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {class: "off", label: "Press"};
+
+        this.press = this.press.bind(this);
+
+        console.log('constructor');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount()');
+    }
+
+    shouldComponentUpdate() {
+        console.log('shouldComponentUpdate()');
+        return true
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate()');
+    }
+
+    // componentWillUnmount(){
+
+    // }
+
+    press() {
+        var className=(this.state.class==="off")?"on":"off";
+        this.setState({class: className});
+    }
+
+    render() {
+        console.log('render()');
+        return <button onClick={this.press} className={this.state.class}>{this.state.label}</button>;
+    }
+
+}
